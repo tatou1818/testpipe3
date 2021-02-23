0c36ca1f9a79526073074c8ede3af36045ee30a2
takwa-slimene <takwa.slimene2019@gmail.com>
2021-02-23 23:29:55 +0100

    pipefailjenkins

---
+++ src/main/java/net/javaguides/springboot/entity/user.java
@@ -0,0 +1,25 @@
+package net.javaguides.springboot.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="user")
+public class user {
+	@Id
+	@GeneratedValue( strategy= GenerationType.IDENTITY )
+	private long id;
+	
+	@Column(name="first_name")
+	private String firstName;
+	
+	@Column(name="last_name")
+	private String lastName;
+	
+	@Column(name="email_id")
+	private String emailId;
+}
