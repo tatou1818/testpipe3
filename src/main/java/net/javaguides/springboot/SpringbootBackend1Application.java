d4c5d0fd1606d51eecc72794eff5e495c94dce98
takwa-slimene <takwa.slimene2019@gmail.com>
2021-02-23 23:48:29 +0100

    change3

--- src/main/java/net/javaguides/springboot/SpringbootBackend1Application.java
+++ src/main/java/net/javaguides/springboot/SpringbootBackend1Application.java
@@ -4,12 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import net.javaguides.springboot.repository.UserRepository;
-
 @SpringBootApplication
-public class SpringbootBackend1Applicationb {
-@Autowired 
-private UserRepository Userre;
+public class SpringbootBackend1Application {
+	@Autowired 
+	private UserRepository Userre;
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootBackend1Application.class, args);
 	}
--- src/main/java/net/javaguides/springboot/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package net.javaguides.springboot.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import net.javaguides.springboot.entity.user;
-
-public interface UserRepository extends JpaRepositoryiiiii<user,Integer> {
-
-}
